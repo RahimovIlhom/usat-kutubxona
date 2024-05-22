fbd3577c48b414d7bc819c4081a7317a60a2596d
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-05-22 13:00:07 +0500

    book models and admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Category
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'book_link', 'create_time', 'isbn']
+    list_filter = ['category']
+    search_fields = ['title', 'category__name', 'author', 'isbn']
+
+
+admin.site.register(Category)
+admin.site.register(Book, BookAdmin)
--- library/models.py
+++ library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Kategoriya")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=Category, related_name='books', verbose_name="Kategoriya")
+    title = models.CharField(max_length=255, verbose_name="Sarlavha")
+    sub_title = models.CharField(max_length=255, null=True, blank=True, verbose_name="Sarlavha osti")
+    author = models.CharField(max_length=255, verbose_name="Avtor")
+    book_link = models.CharField(max_length=255, verbose_name="Kitob manzili")
+    isbn = models.IntegerField(null=True, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="Qo'shilgan vaqt")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="O'zgartirilgan vaqt")
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
