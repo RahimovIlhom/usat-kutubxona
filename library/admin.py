78b096eb685e8cef323680fa45441f2ae9b2339e
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-10-07 10:14:26 +0500

    delete redundant books

--- library/admin.py
+++ library/admin.py
@@ -4,7 +4,7 @@ from .models import Book, Category
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'author', 'book_link', 'book_file']
+    list_display = ['title', 'category', 'author', 'book_file']
     list_filter = ['category']
     search_fields = ['title', 'category__name', 'author', 'isbn']
 
